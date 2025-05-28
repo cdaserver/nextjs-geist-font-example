4bd580560c29bd1083e19638a96a24735813dae1
cdaserver <cdagakpulangpulang@gmail.com>
2025-05-28 18:53:52 +0700

    Add repo-catur2/MODELS/Auth/AuthResponseModel.cs 1748433232349

---
+++ repo-catur2/MODELS/Auth/AuthResponseModel.cs
@@ -0,0 +1,22 @@
+﻿namespace WebApps.MODELS.Auth
+{
+    public class AuthResponseModel
+    {
+        public string? token {  get; set; }
+        public string? user_email {  get; set; }
+        public string? user_nicename { get; set; }
+        public string? user_display_name { get; set; }
+    }
+
+    public class AuthErrResponseModel
+    {
+        public string? code { get; set; }
+        public string? message { get; set; }
+        public AuthErrDataResponseModel? data { get; set; }
+    }
+
+    public class AuthErrDataResponseModel
+    {
+        public int? status { get; set; }
+    }
+}
